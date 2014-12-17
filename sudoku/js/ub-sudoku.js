1cb63c4f9b7f75421ce6a85dda1a9f36b035cf8f
Ardo Kusuma <ardo@snapguide.com>
2014-12-16 19:12:53 -0800

    chore: simplify the board array handling - no need for complex calculation anymore

--- sudoku/js/ub-sudoku.js
+++ sudoku/js/ub-sudoku.js
@@ -1,7 +1,5 @@
 (function ($) {
 
-  var NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
   var SINGLE_TABLE =
     '<table>' +
     '<tbody>' +
@@ -43,7 +41,6 @@
 
   /**
    * possible values of the board
-   * [0..26][0..2]
    *
    * data is from:
    * http://en.wikipedia.org/wiki/File:Sudoku-by-L2G-20050714.svg
@@ -51,17 +48,15 @@
    */
   var BOARD_VALUE = [
     [
-      [5, 3, 4], [6, 7, 8], [9, 1, 2],
-      [6, 7, 2], [1, 9, 5], [3, 4, 8],
-      [1, 9, 8], [3, 4, 2], [5, 6, 7],
-
-      [8, 5, 9], [7, 6, 1], [4, 2, 3],
-      [4, 2, 6], [8, 5, 3], [7, 9, 1],
-      [7, 1, 3], [9, 2, 4], [8, 5, 6],
-
-      [9, 6, 1], [5, 3, 7], [2, 8, 4],
-      [2, 8, 7], [4, 1, 9], [6, 3, 5],
-      [3, 4, 5], [2, 8, 6], [1, 7, 9]
+      [5, 3, 4, 6, 7, 8, 9, 1, 2],
+      [6, 7, 2, 1, 9, 5, 3, 4, 8],
+      [1, 9, 8, 3, 4, 2, 5, 6, 7],
+      [8, 5, 9, 7, 6, 1, 4, 2, 3],
+      [4, 2, 6, 8, 5, 3, 7, 9, 1],
+      [7, 1, 3, 9, 2, 4, 8, 5, 6],
+      [9, 6, 1, 5, 3, 7, 2, 8, 4],
+      [2, 8, 7, 4, 1, 9, 6, 3, 5],
+      [3, 4, 5, 2, 8, 6, 1, 7, 9]
     ]
   ];
 
@@ -70,26 +65,15 @@
    */
   var BOARD_SHOW = [
     [
-      // 0 1 2
-      // 3 4 5
-      // 6 7 8
-      // 9 10 11
-      // 12 13 14
-      // 15 16 17
-      // 18 19 20
-      // 21 22 23
-      // 24 25 26
-      [1, 1, 0], [0, 1, 0], [0, 0, 0],
-      [1, 0, 0], [1, 1, 1], [0, 0, 0],
-      [0, 1, 1], [0, 0, 0], [0, 1, 0],
-
-      [1, 0, 0], [0, 1, 0], [0, 0, 1],
-      [1, 0, 0], [1, 0, 1], [0, 0, 1],
-      [1, 0, 0], [0, 1, 0], [0, 0, 1],
-
-      [0, 1, 0], [0, 0, 0], [1, 1, 0],
-      [0, 0, 0], [1, 1, 1], [0, 0, 1],
-      [0, 0, 0], [0, 1, 0], [0, 1, 1]
+      [1, 1, 0, 0, 1, 0, 0, 0, 0],
+      [1, 0, 0, 1, 1, 1, 0, 0, 0],
+      [0, 1, 1, 0, 0, 0, 0, 1, 0],
+      [1, 0, 0, 0, 1, 0, 0, 0, 1],
+      [1, 0, 0, 1, 0, 1, 0, 0, 1],
+      [1, 0, 0, 0, 1, 0, 0, 0, 1],
+      [0, 1, 0, 0, 0, 0, 1, 1, 0],
+      [0, 0, 0, 1, 1, 1, 0, 0, 1],
+      [0, 0, 0, 0, 1, 0, 0, 1, 1]
     ]
   ];
 
@@ -136,43 +120,25 @@
         var $singleTable = $(SINGLE_TABLE);
         $(this).append($singleTable);
 
-        // get the data for this single table
-//        console.log((ir * 3) + (ir * 6) + ic);
-//        console.log(((ir + 1) * 3) + (ir * 6) + ic);
-//        console.log(((ir + 2) * 3) + (ir * 6) + ic);
-        var firstRow = (ir * 3) + (ir * 6) + ic;
-        var secondRow = (ir + 1) * 3 + (ir * 6) + ic;
-        var thirdRow = (ir + 2) * 3 + (ir * 6) + ic;
-
-        var singleTableData = [
-          self.BOARD_VALUE[firstRow],
-          self.BOARD_VALUE[secondRow],
-          self.BOARD_VALUE[thirdRow]
-        ];
-        var mergedData = [];
-        mergedData = mergedData.concat.apply(mergedData, singleTableData);
-
-        var singleTableShow = [
-          self.BOARD_SHOW[firstRow],
-          self.BOARD_SHOW[secondRow],
-          self.BOARD_SHOW[thirdRow]
-        ];
-        var mergedShow = [];
-        mergedShow = mergedShow.concat.apply(mergedShow, singleTableShow);
-
         $singleTable.find('input').each(function (ip) {
           // ip = index input
 
           var $this = $(this);
           $this.attr('data-row', Math.floor(ip / 3) + (ir * 3));
           $this.attr('data-col', ip % 3 + (ic * 3));
-
-          // only show number that needs to be shown
-          if (mergedShow[ip] === 1) {
-            $this.val(mergedData[ip]).attr('disabled', 'disabled');
-          }
         });
       });
+    });
+
+    // put the values in
+    this.$elem.find('input').each(function () {
+      var $this = $(this);
+      var row = parseInt($this.attr('data-row'));
+      var col = parseInt($this.attr('data-col'));
+
+      if (self.BOARD_SHOW[row][col] === 1) {
+        $this.val(self.BOARD_VALUE[row][col]).attr('disabled', 'disabled');
+      }
     });
 
   };
@@ -192,9 +158,7 @@
       var row = parseInt($this.attr('data-row'));
       var col = parseInt($this.attr('data-col'));
 
-      var obj = self._translateToBoardValIndex(row, col);
-      if (parseInt($this.val()) !==
-        self.BOARD_VALUE[obj.row][obj.col]) {
+      if (parseInt($this.val()) !== self.BOARD_VALUE[row][col]) {
         $this.addClass('error');
         isError = true;
       } else {
@@ -210,13 +174,6 @@
     }
   };
 
-  UberSudoku.prototype._translateToBoardValIndex = function (row, col) {
-    return {
-      row: (row * 3) + Math.floor(col / 3),
-      col: Math.floor(col % 3)
-    };
-  };
-
   UberSudoku.prototype.cheatFill = function () {
     // just fill the answer in
     var self = this;
@@ -224,11 +181,8 @@
       var $this = $(this);
       var row = parseInt($this.attr('data-row'));
       var col = parseInt($this.attr('data-col'));
-
-      var obj = self._translateToBoardValIndex(row, col);
-      $this.val(self.BOARD_VALUE[obj.row][obj.col]);
+      $this.val(self.BOARD_VALUE[row][col]);
     });
-
   };
 
 
