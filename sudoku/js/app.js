a005c43b47e34da32d3299ea4626a8c1234e4386
Ardo Kusuma <ardo@snapguide.com>
2014-12-18 09:40:32 -0800

    chore: use data-api initialization for `uberSudoku` module - update `validate` method (this is not final, will be updated again later)

--- sudoku/js/app.js
+++ sudoku/js/app.js
@@ -1,10 +1,3 @@
 $(function () {
 
-  var $sudokuTable = $('table#sudoku-table');
-  $sudokuTable.uberSudoku();
-
-  $('.cheat-fill-btn').on('click', function () {
-    $sudokuTable.uberSudoku('cheatFill');
-  });
-
 });
--- sudoku/js/ub-sudoku.js
+++ sudoku/js/ub-sudoku.js
@@ -97,10 +97,26 @@
     this.BOARD_VALUE = BOARD_VALUE[index];
     this.BOARD_SHOW = BOARD_SHOW[index];
 
-    this.init();
+    this.matrixCache = [];
+    // initialize the array to be 9x9
+    var i = 0;
+    var j = 0;
+    for (i = 0; i < 9; i++) {
+      this.matrixCache.push([]);
+      for (j = 0; j < 9; j++) {
+        this.matrixCache[i].push(0);
+      }
+    }
+    // now matrixCache is [[], [], []...] 9 arrays with 9 values in each
+
+    this._init();
   }
 
-  UberSudoku.prototype.init = function () {
+  UberSudoku.prototype._rowColTo81 = function (row, col) {
+    return (row * 9) + col;
+  };
+
+  UberSudoku.prototype._init = function () {
     // don't need to initialize if element length is 0 or
     //   data `uber.sudoku` already exists
     if (this.$elem.length === 0 ||
@@ -124,8 +140,10 @@
           // ip = index input
 
           var $this = $(this);
-          $this.attr('data-row', Math.floor(ip / 3) + (ir * 3));
-          $this.attr('data-col', ip % 3 + (ic * 3));
+          var row = Math.floor(ip / 3) + (ir * 3);
+          var col = (ip % 3) + (ic * 3);
+          $this.attr('data-row', row);
+          $this.attr('data-col', col);
         });
       });
     });
@@ -143,37 +161,136 @@
 
   };
 
+  UberSudoku.prototype.generateRandomData = function () {
+
+  };
+
+  UberSudoku.prototype.setCellValue = function (row, col, val) {
+    var target = 'input[data-row=' + row + '][data-col=' + col + ']';
+    this.$elem.find(target).val(val);
+  };
+
+
+  UberSudoku.prototype.checkRow = function (rowIndex) {
+  };
+
+  UberSudoku.prototype.checkColumn = function (colIndex) {
+
+  };
+
+  UberSudoku.prototype.checkCell = function (rowIndex, colIndex) {
+
+  };
+
+  UberSudoku.prototype._getErrorIndexes = function (isRowCheck) {
+    var i = 0;
+    var j = 0;
+    var target = null;
+    var $target = null;
+    var value = 0;
+    var inputs = [];
+    var errorIndexes = [];
+
+    // this is our main validation loop check
+    for (i = 0; i < 9; i++) {
+      for (j = 0; j < 9; j++) {
+        if (isRowCheck) {
+          target = 'input[data-row=' + i + '][data-col=' + j + ']';
+        } else {
+          target = 'input[data-col=' + i + '][data-row=' + j + ']';
+        }
+        $target = this.$elem.find(target);
+        value = $target.val();
+
+        if (value !== '' && inputs.indexOf(value) > -1) {
+          // input already exists, push the error index and break this loop
+          errorIndexes.push(i);
+          break;
+        } else {
+          // input does not exist yet, push it in
+          inputs.push(value);
+        }
+      }
+
+      // for each iteration, reset `inputs`
+      inputs = [];
+    }
+
+    return errorIndexes;
+  };
+
   /**
+   * Clear all error classes
+   */
+  UberSudoku.prototype._clearErrors = function () {
+    this.$elem.find('table').removeClass('error');
+    this.$elem.find('input').removeClass('error');
+  };
+
+  /**
+   * Each table section cannot have duplicate number
    * Each row and each column cannot have any duplicate number
    */
   UberSudoku.prototype.validate = function () {
-    // TODO HACK uber ugly check, just use the array....
+    // clear out all error classes first
+    this._clearErrors();
 
-    var self = this;
-    var isError = false;
+    // check each table section first
+    var hasTableSectionError = false;
 
-    this.$elem.find('input').each(function () {
-      var $this = $(this);
-      $this.removeClass('success');
-      var row = parseInt($this.attr('data-row'));
-      var col = parseInt($this.attr('data-col'));
+    this.$elem.find('table').each(function () {
 
-      if (parseInt($this.val()) !== self.BOARD_VALUE[row][col]) {
-        $this.addClass('error');
-        isError = true;
-      } else {
-        $this.removeClass('error');
+      var $thisTable = $(this);
+      var inputs = [];
+
+      // we want to check that each table has unique input
+      $thisTable.find('input').each(function () {
+
+        var value = $(this).val();
+        if (value !== '' && inputs.indexOf(value) > -1) {
+          hasTableSectionError = true;
+          return false; // this is how we break jQuery `each`
+        }
+        inputs.push(value);
+
+      });
+
+      if (hasTableSectionError) {
+        $thisTable.addClass('error');
       }
+
     });
 
-    if (!isError) {
+    // get the error indexes for row and column
+    var errorRowIndexes = this._getErrorIndexes(true);
+    var errorColIndexes = this._getErrorIndexes(false);
+
+    // now add error class to each input that has error
+    var index = 0;
+    for (index = 0; index < errorRowIndexes.length; index++) {
+      this.$elem.find('input[data-row=' + errorRowIndexes[index] + ']').each(function () {
+        $(this).addClass('error');
+      });
+    }
+    for (index = 0; index < errorColIndexes.length; index++) {
+      this.$elem.find('input[data-col=' + errorColIndexes[index] + ']').each(function () {
+        $(this).addClass('error');
+      });
+    }
+
+    // check whether there's any error exists
+    if (!hasTableSectionError && errorRowIndexes.length === 0 && errorColIndexes.length === 0) {
       // hooray, no error
       this.$elem.find('input').each(function () {
         $(this).addClass('success');
       });
     }
+
   };
 
+  /**
+   * Method to fill all cells right away
+   */
   UberSudoku.prototype.cheatFill = function () {
     // just fill the answer in
     var self = this;
@@ -184,7 +301,6 @@
       $this.val(self.BOARD_VALUE[row][col]);
     });
   };
-
 
   var old = $.fn.uberSudoku;
 
@@ -204,15 +320,34 @@
     });
   };
 
+  /**
+   * call this if you want to use the previous `uberSudoku`
+   */
   $.fn.uberSudoku.noConflict = function () {
     $.fn.uberSudoku = old;
     return this;
   };
 
+  // DATA-API CALL
+  // =============
+
+  $(function () {
+    $('[data-uber-sudoku]').uberSudoku();
+  });
+
+
+  // DATA-API EVENT LISTENERS
+  // ========================
+
   $(document).on('click.ub.sudoku', '[data-ub-trigger=validate]', function () {
     var data = $($(this).data('ubTarget')).data('uber.sudoku');
     if (data) {
       data.validate();
+    }
+  }).on('click.ub.sudoku', '[data-ub-trigger=cheatFill]', function () {
+    var data = $($(this).data('ubTarget')).data('uber.sudoku');
+    if (data) {
+      data.cheatFill();
     }
   });
 
